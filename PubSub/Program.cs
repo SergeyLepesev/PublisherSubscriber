abb2d2ea3124056ff8664de5a159aa9499751208
Sergey Lepesev <lepesevss@yandex.ru>
2018-03-18 20:55:05 +0500

    class Sub = Subscriber

--- PubSub/Program.cs
+++ PubSub/Program.cs
@@ -6,7 +6,31 @@ namespace PubSub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack<int>();
+            var sub = new Sub<int>(stack);
+            stack.Push(1);
+            stack.Pop();
+            Console.ReadKey();
+
+        }
+
+        public class Sub<T>
+        {
+            public Sub(Stack<T> stack)
+            {
+                stack.NotifyPop += ConsoleNotifyPop;
+                stack.NotifyPush += ConsoleNotifyPush;
+            }
+
+            public static void ConsoleNotifyPop(object sender, EventArgs e)
+            {
+                Console.WriteLine("Use pop");
+            }
+
+            public static void ConsoleNotifyPush(object sender, EventArgs e)
+            {
+                Console.WriteLine("Use push");
+            }
         }
     }
 }
