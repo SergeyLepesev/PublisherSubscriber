8ae22c2b863a7584323aedd54d91cbba08de9698
Sergey Lepesev <lepesevss@yandex.ru>
2018-03-18 20:48:45 +0500

    add Stack = Publisher

---
+++ PubSub/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PubSub
+{
+    public class StackNode<T>
+    {
+        public T Item { get; set; }
+        public StackNode<T> Next { get; set; }
+        public StackNode(T item)
+        {
+            Item = item;
+        }
+    }
+
+    public class Stack<T> : IEnumerable<T>
+    {
+        public event EventHandler NotifyPop;
+        public event EventHandler NotifyPush;
+
+        private StackNode<T> _head;
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            var node = new StackNode<T>(item);
+            node.Next = _head;
+            _head = node;
+            Count++;
+            NotifyPush?.Invoke(this, EventArgs.Empty);
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+                throw new Exception("stack empty");
+            StackNode<T> node = _head;
+            _head = _head.Next;
+            Count--;
+            NotifyPop?.Invoke(this, EventArgs.Empty);
+            return node.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            StackNode<T> node = _head;
+            while (node != null)
+            {
+                yield return node.Item;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
